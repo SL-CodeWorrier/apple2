83c79a0e2535b7ac2aa8ce1cbb7b26a6e139e2f8
SL-CodeWorrier <chathuathnayaka@gmail.com>
2024-03-14 12:39:29 +0530

    commit2

--- hottest/ContentView.swift
+++ hottest/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, woshika!")
+            Text("Hello, woshika! hello maththaka gamge")
         }
         .padding()
     }
