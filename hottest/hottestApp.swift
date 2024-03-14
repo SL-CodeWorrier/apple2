a8ec0f69f5b3ac9540f04036b2087fceac292881
SL-CodeWorrier <chathuathnayaka@gmail.com>
2024-03-14 12:36:42 +0530

    Initial Commit

---
+++ hottest/hottestApp.swift
@@ -0,0 +1,17 @@
+//
+//  hottestApp.swift
+//  hottest
+//
+//  Created by Chathura Aththanayaka on 2024-03-14.
+//
+
+import SwiftUI
+
+@main
+struct hottestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
